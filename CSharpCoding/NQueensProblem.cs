a97ab26c15ab2c5a012136d2a94ab5271499a57c
Sanjib Das <sanjibdas86@gmail.com>
2021-04-24 12:12:57 +0530

    Added NQueen Peoblem

---
+++ CSharpCoding/NQueensProblem.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCoding
+{
+    public class NQueensProblem
+    {
+        int noOfQueens = 0;
+        int[,] chessTable;
+
+        
+        public void Solve(int noOfQueens)
+        {
+            this.noOfQueens = noOfQueens;
+            chessTable = new int[noOfQueens, noOfQueens];
+
+            if ( SetQueens(0))
+            {
+                PrintChessTable();
+            }
+            else
+            {
+                Console.WriteLine("No Valid solution exists");
+            }
+
+        }
+
+        bool SetQueens(int n)
+        {
+            if (n >= noOfQueens) return true;
+
+            for(int i=0; i<noOfQueens; i++)
+            {
+                if(IsValidPosition(i,n))
+                {
+                    chessTable[i, n] = 1;
+                    if(SetQueens(n+1))
+                    {
+                        return true;
+                    }
+
+                    chessTable[i, n] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsValidPosition(int row, int col)
+        {
+            for(int i = 0; i <col; i++)
+            {
+                if (chessTable[row, i] == 1) return false;
+            }
+
+            for (int i = col, j = row; i >= 0 && j < noOfQueens; i--,j++)
+            {
+                if(chessTable[j, i] == 1) return false;
+            }
+
+            for (int i = col, j = row; i >= 0 && j >= 0; i--, j--)
+            {
+                if(chessTable[j, i] == 1) return false;
+            }
+
+            return true;
+        }
+
+        void PrintChessTable()
+        {
+            for(int row = 0; row<noOfQueens; row++)
+            {
+                for(int col = 0; col < noOfQueens; col++)
+                {
+                    if(chessTable[row,col] == 1)
+                    Console.Write("x ");
+                    else Console.Write("- ");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- CSharpCoding/Program.cs
+++ CSharpCoding/Program.cs
@@ -10,6 +10,21 @@ namespace CSharpCoding
     {
         static void Main(string[] args)
         {
+            NQueensProblemTest();
+
+            Console.ReadKey();
+            Console.WriteLine("Press any key to exit");
+        }
+
+        private static void NQueensProblemTest()
+        {
+            NQueensProblem nQueensProblem = new NQueensProblem();
+
+            nQueensProblem.Solve(5);
+
+            nQueensProblem.Solve(8);
+
+            nQueensProblem.Solve(20);
         }
     }
 }
